8731c24d7edca3997a5b0205e9737010dbd0cacf
Ironman143000 <aniketsonwane34264@gmail.com>
2026-03-11 03:07:56 +0530

    fix aws configure

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,14 +3,37 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name = "github-actions-eks"
+  cluster_name = var.cluster_name
 
-  cluster_version = "1.33"
+  cluster_version = var.cluster_version
 
   vpc_id = module.vpc.vpc_id
 
   subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
+
+}
+
+module "node_group" {
+
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  version = "~> 20.0"
+
+  cluster_name = module.eks.cluster_name
+
+  name = "github-node-group"
+
+  subnet_ids = module.vpc.private_subnets
+
+  instance_types = ["t3.medium"]
+
+  ami_type = "AL2023_x86_64_STANDARD"
+
+  desired_size = 2
+  min_size     = 1
+  max_size     = 3
+
+  capacity_type = "ON_DEMAND"
 
 }
